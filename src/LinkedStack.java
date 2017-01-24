7c9e8e2d0f5829d2a5d805fbb476acff167ddf17
Joshua Kennedy <joshkennede@gmail.com>
2017-01-24 11:59:48 -0800

    Added getter/setters to private inner class Node

--- src/LinkedStack.java
+++ src/LinkedStack.java
@@ -8,7 +8,33 @@ public class LinkedStack<T> implements StackInterface<T> {
           topNode = null;
      } // end default constructor
 
+     private class Node {
+          private T data; // entry in stack
+          private Node next; // link to get next node
+
+          private Node(T dataPortion) {
+               this(dataPortion, null);
+          } //end constructor
+          private Node(T dataPortion, Node nextNode) {
+               data = dataPortion;
+               next = nextNode;
+          } // end constructor
+          private T getData() {
+               return data;
+          } // end getData
+          private void setData(T newData) {
+               data = newData;
+          } // end setData
+          private Node getNextNode() {
+               return next;
+          } // end getNextNode
+          private void setNextNode(Node nextNode) {
+               next = nextNode;
+          } // end setNextNode
+     } // end Node
+
      public void push(T anEntry) {
+          T newEntry = null;
           Node newNode = new Node(newEntry, topNode);
           topNode = newNode;
      } // end push
@@ -34,9 +60,4 @@ public class LinkedStack<T> implements StackInterface<T> {
      public void clear() {
           topNode = null;
      } // end clear
-
-     private class Node {
-          private T data; // entry in stack
-          private Node next; // link to get next node
-     } // end Node
 } // end LinkedStack
