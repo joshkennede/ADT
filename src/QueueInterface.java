014c4e9514ea8c42867323373b16bade62d6d7f1
Joshua Kennedy <joshkennede@gmail.com>
2017-01-24 16:29:30 -0800

    QueueInterface

---
+++ src/QueueInterface.java
@@ -0,0 +1,37 @@
+/**
+ * Created by JoshuaK on 1/24/2017.
+ */
+public interface QueueInterface<T> {
+
+     /**
+      * Adds a new entry to the back of the queue
+      * @param newEntry an object to be added
+      */
+     public void enqueue(T newEntry);
+
+     /**
+      * Removes and returns the entry at the front of this queue.
+      * @return either the object at the front of the queue or,
+      * if the queue is empty before the operation, null
+      */
+     public T dequeue();
+
+     /**
+      * Retrieves the entry at the front of the queue
+      * @return either the object at the front of the queue or,
+      * if the queue is empty, null
+      */
+     public T getFront();
+
+     /**
+      * Detects whether this queue is empty
+      * @return true if the queue is empty, false otherwise
+      */
+     public boolean isEmpty();
+
+     /**
+      * Removes all entries from this queue
+      */
+     public void clear();
+
+} // end QueueInterface
